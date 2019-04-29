d1ddb1a7f7f57cffe4c74c15e34bea763a306608
Danny Brown <dannybrown37@gmail.com>
2019-04-28 21:43:23 -0400

    PUT and POST requests still kind of a mess, but hopefully getting somewhere...

--- app.py
+++ app.py
@@ -49,7 +49,6 @@ def create_service():
     if not request.json or not 'name' in request.json:
         abort(400)
     icon_ids = []
-    icon_records = []
     for icon in request.json["icons"]:
         new_icon = {
             "icon" : icon["icon"],
@@ -57,14 +56,12 @@ def create_service():
         }
         new_icon_record = icons_object.insert(new_icon)
         icon_ids.append(new_icon_record["id"])
-        icon_records.append(new_icon_record)
-    new_icons_object = create_icons_object() # refresh so we get new ones
+    refreshed_icons_object = create_icons_object() # get the new ones
     service = {
         # id is handled by airtable automatically
         "Name": request.json["name"],
         "Desc": request.json.get("desc", ""),
-        "Icons": [icon_record for icon_record in icon_records] 
-                 # [new_icons_object.get(id) for id in icon_ids]
+        "Icons": [refreshed_icons_object.get(id) for id in icon_ids]
     }
     services_object.insert(service)
     return jsonify({'service': make_public_service(service)}), 201
@@ -82,24 +79,34 @@ def delete_service(service_id):
 # PUT
 @app.route('/cards/api/services/<int:service_id>', methods=['PUT'])
 @auth.login_required
-def update_task(service_id):
-    service = [s for s in services if s['id'] == service_id]
-    if len(service) == 0:
+def update_service(service_id):
+    service = services_object.match('ID', str(service_id))
+    if not service:
         abort(404)
     if not request.json:
         abort(400)
-    if 'name' in request.json and type(request.json['name']) != unicode:
+    """
+    if 'name' in request.json and type(request.json['name']) != type(unicode):
         abort(400)
-    if 'desc' in request.json and type(request.json['desc']) is not unicode:
+    if 'desc' in request.json and type(request.json['desc']) is not type(unicode):
         abort(400)
     # TODO make this work
     if 'icons' in request.json and type(request.json['icons']) is not type(list):
         abort(400)
-    service[0]['name'] = request.json.get('name', service[0]['name'])
-    service[0]['desc'] = request.json.get('desc', service[0]['desc'])
-    # TODO make this work
-    service[0]['icons'] = request.json.get('icons', service[0]['icons'])
-    return jsonify({'service': make_public_service(service[0])})
+    """
+    update_fields = {
+        "Name" : request.json["name"] if "name" in request.json else "",
+        "Desc" : request.json["desc"] if "desc" in request.json else "",
+        "Icons" : [] # TODO make icons work
+    }
+    update_fields = {
+        key: value for key, value in update_fields.items() if value is not None
+    }
+    print(update_fields)
+    wait = input("waiting...\n\n\n")
+    services_object.replace(service['id'], jsonify(update_fields))
+    wait = input("waiting...\n\n\n")
+    return jsonify({'service': make_public_service(service)})
 
 # ERRORS returned as JSON
 @app.errorhandler(404)
